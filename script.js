e676f0c464075d1599f278db67c549b145ef1f95
Innuendo416 <collinstone2291@icloud.com>
2021-02-25 15:40:07 -0500

    React has spoiled me way too much

--- script.js
+++ script.js
@@ -2,16 +2,23 @@
 
 document.getElementById('nav__dropBtn').addEventListener('click', (e) => {
 	e.stopPropagation()
-	document.getElementById('dropDown').classList.add('dropDown--active')
+	const dropDown = document.getElementById('dropDown')
+	dropDown.classList.add('dropDown--active')
 	gsap.fromTo('#dropDown', {x: '100%'}, {x: '0%', duration: 0.25})
 
 	const closeDropDown = (e) => {
-		if(!document.getElementById('dropDown').contains(e.target) || e.target.matches('.dropDown__closeBtn')){
-			gsap.fromTo('#dropDown', {x: '0%'}, {x: '100%', duration: 0.25})
-			setTimeout(() => document.getElementById('dropDown').classList.remove('dropDown--active'), 500);
-			
-		}
+		gsap.fromTo('#dropDown', {x: '0%'}, {x: '100%', duration: 0.25})
+		setTimeout(() => dropDown.classList.remove('dropDown--active'), 500);
+		return () => window.removeEventListener('click', closeDropDown)
 	}
-	window.addEventListener('click', closeDropDown)
+
+	const clickedOutside = (e) => {
+		if(!dropDown.contains(e.target)){
+			closeDropDown()
+		}
+		return () => window.removeEventListener('click', clickedOutside)
+	}
+	document.getElementById('dropDown__closeBtn').addEventListener('click', closeDropDown)	
+	window.addEventListener('click', clickedOutside)
 
 })
