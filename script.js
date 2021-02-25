d2cafafac2ce1f17ce6c151cd32d6b08f181afc8
Innuendo416 <collinstone2291@icloud.com>
2021-02-25 14:26:49 -0500

    ...

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-document.getElementById('nav__dropBtn').addEventListener('click', () => {
+document.getElementById('nav__dropBtn').addEventListener('mousedown', () => {
 
 	document.getElementById('dropDown').classList.add('dropDown--active')
 	gsap.fromTo('#dropDown', {x: '100%'}, {x: '0%', duration: 0.25})
@@ -9,9 +9,9 @@ document.getElementById('nav__dropBtn').addEventListener('click', () => {
 		if(e.target.matches('.dropDown__closeBtn')){
 			gsap.fromTo('#dropDown', {x: '0%'}, {x: '100%', duration: 0.25})
 			setTimeout(() => document.getElementById('dropDown').classList.remove('dropDown--active'), 500);
-			return () => window.removeEventListener('click', closeDropDown)
+			return () => window.removeEventListener('mousedown', closeDropDown)
 		}
 	}
-	window.addEventListener('click', closeDropDown)
+	window.addEventListener('mousedown', closeDropDown)
 
 })
