4b7a3c9ece5794448cdc4496a9a39edcdf86e1b2
Innuendo416 <collinstone2291@icloud.com>
2021-02-25 14:21:07 -0500

    Stupid mobile doesnt support gap

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ document.getElementById('nav__dropBtn').addEventListener('click', () => {
 	const closeDropDown = (e) => {
 		if(e.target.matches('.dropDown__closeBtn')){
 			gsap.fromTo('#dropDown', {x: '0%'}, {x: '100%', duration: 0.25})
-			document.getElementById('dropDown').style.background ='black'
 			setTimeout(() => document.getElementById('dropDown').classList.remove('dropDown--active'), 500);
 			return () => window.removeEventListener('click', closeDropDown)
 		}
